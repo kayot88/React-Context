8eb1cc0f27e90bb331bca31e9f9fdbb33e9c5ac6
Kayot88 <kayot88@gmail.com>
2019-04-10 16:09:47 +0300

    Context API Explained

--- src/App.js
+++ src/App.js
@@ -1,66 +1,66 @@
 import React, { Component } from 'react';
 
-// first we will make a new context
 const MyContext = React.createContext();
-
-// Then create a provider Component
 class MyProvider extends Component {
   state = {
-    name: 'Wes',
+    name: 'gromov',
     age: 100,
     cool: true
-  }
+  };
   render() {
     return (
-      <MyContext.Provider value={{
-        state: this.state,
-        growAYearOlder: () => this.setState({
-          age: this.state.age + 1
-        })
-      }}>
+      <MyContext.Provider
+        value={{
+          state: this.state,
+          updateState: () => {
+            return this.setState({
+              age: this.state.age + 1
+            });
+          }
+        }}
+      >
         {this.props.children}
       </MyContext.Provider>
-    )
+    );
   }
 }
 
-const Family = (props) => (
+const Family = props => (
   <div className="family">
     <Person />
   </div>
-)
-
-class Person extends Component {
-  render() {
-    return (
-      <div className="person">
-        <MyContext.Consumer>
-          {(context) => (
-            <React.Fragment>
-              <p>Age: {context.state.age}</p>
-              <p>Name: {context.state.name}</p>
-              <button onClick={context.growAYearOlder}>🍰🍥🎂</button>
-            </React.Fragment>
-          )}
-        </MyContext.Consumer>
-      </div>
-    )
-  }
-}
-
+);
 
 class App extends Component {
   render() {
     return (
       <MyProvider>
         <div>
-          <p>I'm the app</p>
+          <p className="first">i`m a first comp</p>
           <Family />
         </div>
       </MyProvider>
     );
   }
 }
-
+class Person extends Component {
+  render() {
+    return (
+      <div className="second">
+        <MyContext.Consumer>
+          {context => (
+            <React.Fragment>
+              <p>name: {context.state.name}</p>
+              <p>age: {context.state.age}</p>
+              <button onClick={context.updateState}>
+                Update state
+              </button>
+            </React.Fragment>
+          )}
+        </MyContext.Consumer>
+      </div>
+    );
+  }
+}
 
 export default App;
